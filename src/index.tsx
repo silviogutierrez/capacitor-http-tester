a01a9b0bd9e877c59c08e5651930428ea9fb6df1
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2022-12-16 22:12:54 -0500

    Basic example is working

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,17 @@
-alert('ACTUALLY WORKING');
+import * as React from 'react'
+import {createRoot} from "react-dom/client";
+
+const root = createRoot(document.getElementById("root")!);
+
+const testing = async () => {
+    const response = await fetch("https://www.joyapp.com/api/credentials/");
+    alert(JSON.stringify(await response.json(), null, 2));
+}
+
+root.render(
+    <div>
+        <button onClick={testing}>
+            Click me
+        </button>
+    </div>,
+);
