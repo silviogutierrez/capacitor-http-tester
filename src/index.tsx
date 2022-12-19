c83855b242ef58479a9637e5e4225622c46e4cc8
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2022-12-18 23:41:18 -0500

    Tests

--- src/index.tsx
+++ src/index.tsx
@@ -11,24 +11,24 @@ const setCookie = (name: string, value: string) => {
 
 const simple = async () => {
     const response = await fetch("http://localhost:3000/api/simple/");
-    alert(JSON.stringify(await response.json()) == '{"thisThing":"Works"}');
+    return JSON.stringify(await response.json()) == '{"thisThing":"Works"}';
 }
 
 const serializingHeaders = async () => {
     const response = await fetch("http://localhost:3000/api/headers/", {headers: new Headers({"X-Some-Header": "Working"})});
     const {header} = await response.json();
-    alert(header == "Working");
+    return header == "Working";
 }
 
 const responseHeaders = async () => {
     const response = await fetch("http://localhost:3000/api/simple/");
-    alert(response.headers instanceof Headers);
+    return response.headers instanceof Headers;
 }
 
 const cookie = async () => {
     const toSet = makeRandomString();
     const response = await fetch(`http://localhost:3000/api/cookie/${toSet}/`);
-    alert(document.cookie.includes(toSet));
+    return document.cookie.includes(toSet);
 }
 
 const urlEncoded = async () => {
@@ -38,7 +38,7 @@ const urlEncoded = async () => {
           }, body: new URLSearchParams(payload)
     })
 
-    alert(JSON.stringify(await response.json()) == '{"some":"value"}');
+    return JSON.stringify(await response.json()) == '{"some":"value"}';
 }
 
 const multipartEncoded = async () => {
@@ -47,32 +47,32 @@ const multipartEncoded = async () => {
     const response = await fetch(`http://localhost:3000/api/multipart/`, {method: "POST", body});
 
     const {result} = await response.json();
-    alert(result == "itdoes");
+    return result == "itdoes";
 }
 
 const numberResponse = async () => {
     const response = await fetch(`http://localhost:3000/api/number/`);
-    alert(await response.json() == 5);
+    return await response.json() == 5;
 }
 
 const stringResponse = async () => {
     const response = await fetch(`http://localhost:3000/api/string/`);
-    alert(await response.json() == "a string");
+    return await response.json() == "a string";
 }
 
 const nullResponse = async () => {
     const response = await fetch(`http://localhost:3000/api/null/`);
-    alert(await response.json() == null);
+    return await response.json() == null;
 }
 
 const trueResponse = async () => {
     const response = await fetch(`http://localhost:3000/api/true/`);
-    alert(await response.json() == true);
+    return await response.json() == true;
 }
 
 const falseResponse = async () => {
     const response = await fetch(`http://localhost:3000/api/false/`);
-    alert(await response.json() == false);
+    return await response.json() == false;
 }
 
 const readCookie = async () => {
@@ -82,47 +82,59 @@ const readCookie = async () => {
 
     const response = await fetch(`http://localhost:3000/api/read-cookie/${cookieName}`);
     const {value} = await response.json();
-    alert(value == cookieValue);
+    return value == cookieValue;
 }
 
 const readBlob = async () => {
     const response = await fetch("http://localhost:3000/api/blob/");
     const {size} = await response.blob();
-    alert(size == 33788);
+    return size == 33788;
 }
 
 const badRequest = async () => {
     const response = await fetch("http://localhost:3000/api/400/");
     const {thisField} = await response.json();
-    alert(response.status == 400 && thisField == "is required");
+   return response.status == 400 && thisField == "is required";
 }
 
 const networkError = async () => {
     try {
         const response = await fetch("https://nowaythisworks");
-        alert(false);
+        return false;
     }
     catch (error: unknown) {
-        alert(true);
+        return true;
     }
 }
 
+type TestCase = () => Promise<boolean>;
+
+const Test = (props: {name: string; test: TestCase}) => {
+    const [passed, setPassed] = React.useState(false);
+
+    React.useEffect(() => {
+            props.test().then((passed) => {
+                setPassed(passed);
+            }).catch(() => setPassed(false));
+    }, []);
+
+    return <div style={{lineHeight: 1}}>
+        <h3 style={{marginTop: 0, marginBottom: 0}}>{props.name}</h3>
+        {passed == true && <h5 style={{marginTop: 0, marginBottom: 0, color: "green"}}>Passed</h5>}
+        {passed == false && <h5 style={{marginTop: 0, marginBottom: 0, color: "red"}}>Failed</h5>}
+    </div>;
+}
+
+const Tests = (props: {tests: Record<string, TestCase>}) => {
+    return <div>
+        {Object.entries(props.tests).map(([name, test]) => {
+            return <Test key={name} name={name}test={test} />
+        })}
+        </div>;
+};
+
 root.render(
     <div>
-        <button onClick={simple}>Simple</button>
-        <button onClick={serializingHeaders}>Headers in request</button>
-        <button onClick={responseHeaders}>Response headers</button>
-        <button onClick={cookie}>Cookie</button>
-        <button onClick={urlEncoded}>URL Encoded</button>
-        <button onClick={multipartEncoded}>Multipart Encoded</button>
-        <button onClick={numberResponse}>Number response</button>
-        <button onClick={stringResponse}>String response</button>
-        <button onClick={nullResponse}>Null response</button>
-        <button onClick={trueResponse}>True response</button>
-        <button onClick={falseResponse}>False response</button>
-        <button onClick={readCookie}>Read Cookie</button>
-        <button onClick={readBlob}>Read Blob</button>
-        <button onClick={badRequest}>Bad request</button>
-        <button onClick={networkError}>Network error</button>
+        <Tests tests={{simple, serializingHeaders, responseHeaders, cookie, urlEncoded, multipartEncoded, numberResponse, stringResponse, nullResponse, trueResponse, falseResponse, readCookie, readBlob, badRequest, networkError}} />
     </div>,
 );
